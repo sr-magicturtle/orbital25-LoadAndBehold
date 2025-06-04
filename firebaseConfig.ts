1259eb8aa4dd3d95354dcda5ff6e6dae74504c94
sr-magicturtle <neoshaoray@gmail.com>
2025-06-04 16:59:25 +0800

    add firebaseConfig.js

---
+++ firebaseConfig.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXEra3oQVlDTfxpF541hyBlyPvCivymac",
+  authDomain: "washerwatcher-11e05.firebaseapp.com",
+  projectId: "washerwatcher-11e05",
+  storageBucket: "washerwatcher-11e05.firebasestorage.app",
+  messagingSenderId: "96533122149",
+  appId: "1:96533122149:web:c0385f587c9d57033a0a8c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
